15d3e4edffb8c0085a8c855bcdbddb47919729b2
trang <hd.oang.ong@gmail.com>
2019-12-08 10:32:53 +0700

    add Bai 3

--- Bai3-Query-parameters/index.js
+++ Bai3-Query-parameters/index.js
@@ -9,15 +9,26 @@ app.set('views','./views');
 app.get('/',function(req,res){
     res.render("index");
 });
+
+var users=[
+    {id: 1,name: 'trang'},
+    {id: 2,name: 'Thanh'},
+    {id: 3, name:'Tinh'}
+    ];
 app.get('/users',function(req,res){
     res.render("./users",{
-        users:[
-        {id: 1,name: 'trang'},
-        {id: 2,name: 'Thanh'},
-        {id: 3, name:'Tinh'}
-        ]
+        users:users
     });
+});
 
+app.get('/users/search', function(req,res){
+    var q=req.query.q;
+    var matchedUsers=users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase())!==-1;// nếu k có trong danh sách thì trả về -1    
+    });
+    res.render('./users/index',{
+        users:matchedUsers
+    })
 });
 app.listen(port,function(){
     console.log('port :'+port);
