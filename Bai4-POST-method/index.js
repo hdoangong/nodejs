1d889ec1847b8379fa2d0d935f541a0953cd0927
trang <hd.oang.ong@gmail.com>
2019-12-08 11:43:18 +0700

    add bai 4

--- Bai4-POST-method/index.js
+++ Bai4-POST-method/index.js
@@ -20,7 +20,6 @@ app.get('/users',function(req,res){
         users:users
     });
 });
-
 app.get('/users/search', function(req,res){
     var q=req.query.q;
     var matchedUsers=users.filter(function(user){
@@ -30,6 +29,25 @@ app.get('/users/search', function(req,res){
         users:matchedUsers
     })
 });
+
+//bai 4:
+
+var bodyParser=require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/users/create',function(req,res){
+    res.render('./users/create');
+});
+
+app.post('/users/create',function(req,res){
+    // console.log(req.body);
+    users.push(req.body);
+    res.redirect('/users');// chuyển về trang 
+})
+
+
 app.listen(port,function(){
     console.log('port :'+port);
 })
