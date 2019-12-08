dee9f6e48a97bb60e29e83ed4f8859ede3b266e9
trang <hd.oang.ong@gmail.com>
2019-12-08 13:20:18 +0700

    create Bai 7 clone Bai 6

---
+++ Bai7-View-user/index.js
@@ -0,0 +1,66 @@
+var express=require('express');
+var app=express();
+var port=3000;
+
+var low =require('lowdb');
+var FileSync=require('lowdb/adapters/FileSync');
+var adapter=new FileSync('db.json');
+db=low(adapter);
+db.defaults({users:[]})
+.write();
+
+
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+
+app.get('/',function(req,res){
+    res.render("index");
+});
+
+var a=[
+    {id: 1,name: 'trang'},
+    {id: 2,name: 'Thanh'},
+    {id: 3, name:'Tinh'}
+    ];
+app.get('/users',function(req,res){
+    res.render("./users",{
+        users:db.get('users').value()//users
+    });
+});
+app.get('/users/search', function(req,res){
+    var q=req.query.q;
+    var matchedUsers=db.get('users').value();
+    matchedUsers = matchedUsers.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase())!==-1;// nếu k có trong danh sách thì trả về -1    
+    })
+    // var matchedUsers=users.filter(function(user){
+    //     return user.name.toLowerCase().indexOf(q.toLowerCase())!==-1;// nếu k có trong danh sách thì trả về -1    
+    // });
+    res.render('./users/index',{
+        users:matchedUsers
+    })
+});
+
+//bai 4:
+
+var bodyParser=require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/users/create',function(req,res){
+    res.render('./users/create');
+});
+
+app.post('/users/create',function(req,res){
+    // console.log(req.body);
+    db.get('users').push(req.body).write();//users.push(req.body);
+    res.redirect('/users');// chuyển về trang 
+});
+
+
+app.listen(port,function(){
+    console.log('port :'+port);
+});
